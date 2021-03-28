6853e2fd1df05e9d4e95ddaffff3e42aa8752432
robert_bao <ibaochen@gmail.com>
2021-03-28 08:42:38 -0400

    Frontend

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,144 +9,144 @@ import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
-import Button from "@material-ui/core/Button";
-import axios from "axios";
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
-  content: {
-    height: "calc(100vh - 64px)",
-  },
-  item: {
-    marginY: theme.spacing(4),
-  },
-  searchbar: {
-    padding: "4px 4px",
-    display: "flex",
-    alignItems: "center",
-    width: 600,
-    backgroundColor: theme.palette.background.default,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
+    content: {
+        height: "calc(100vh - 64px)",
+    },
+    item: {
+        marginY: theme.spacing(4),
+    },
+    searchbar: {
+        padding: "4px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 600,
+        backgroundColor: theme.palette.background.default,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
 }));
 
 export default function Transcribe() {
-  const classes = useStyles();
-  const [url, setURL] = useState('');
-  const [vidId, setVidId]=useState('');
+    const classes = useStyles();
+    const [url, setURL] = useState('');
+    const [vidId, setVidId] = useState('');
 
 
-  const handleURLChange = event => {
-		setURL(event.target.value);
-    if(event.target.value.includes('youtu.be')){
-      setVidId(event.target.value.split('.be/')[1])
-      console.log(event.target.value.split('.be/')[1])
-    }
-    else if(event.target.value.includes('v=') && event.target.value.includes("&ab_channel")){
-      setVidId(event.target.value.split('v=')[1].substring(0,event.target.value.split('v=')[1].indexOf('&')));
-      console.log(event.target.value.split('v=')[1].substring(0,event.target.value.split('v=')[1].indexOf('&')))
-    }
-    else if(event.target.value.includes("v=")){
-      setVidId(event.target.value.split("v=")[1])
-      console.log(event.target.value.split("v=")[1])
-    }
-    
-	};
+    const handleURLChange = event => {
+        setURL(event.target.value);
+        if (event.target.value.includes('youtu.be')) {
+            setVidId(event.target.value.split('.be/')[1])
+            console.log(event.target.value.split('.be/')[1])
+        } else if (event.target.value.includes('v=') && event.target.value.includes("&ab_channel")) {
+            setVidId(event.target.value.split('v=')[1].substring(0, event.target.value.split('v=')[1].indexOf('&')));
+            console.log(event.target.value.split('v=')[1].substring(0, event.target.value.split('v=')[1].indexOf('&')))
+        } else if (event.target.value.includes("v=")) {
+            setVidId(event.target.value.split("v=")[1])
+            console.log(event.target.value.split("v=")[1])
+        }
 
-  function handleSubmit(){
-    // Make a GET request with a shorthand method
-    
-    const config = {
-      method: "post",
-      // put the backend endpoint here
-      url: `http://127.0.0.1:8000/summarize/${vidId}`,
-      headers: {},
     };
 
-    axios(config)
-      .then((response) => {
-        // get things from API
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    
-  }
-  
-  return (
-    <Box bgcolor="primary.main" className="classes.content">
-      <Box my={14} mx={2}>
-        <AppBar />
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Grid item className={classes.item}>
-            <Box>
-              <Typography variant="h1">
-                Empower students
-                <br />
-                with ADHD
-              </Typography>
+    function handleSubmit() {
+        // Make a GET request with a shorthand method
+        // const config = {
+        //   method: "post",
+        //   // put the backend endpoint here
+        //   url: `http://127.0.0.1:8000/summarize/${vidId}`,
+        //   headers: {},
+        // };
+        //
+        // axios(config)
+        //     .then((response) => {
+        //       // get things from API
+        //       console.log(response);
+        //     })
+        //     .catch((error) => {
+        //       console.log(error);
+        //     });
+
+    }
+
+    return (
+        <Box bgcolor="primary.main" className="classes.content">
+            <Box my={14} mx={2}>
+                <AppBar/>
+                <Grid
+                    container
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center"
+                >
+                    <Grid item className={classes.item}>
+                        <Box>
+                            <Typography variant="h1">
+                                Empower students
+                                <br/>
+                                with ADHD
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item className={classes.item}>
+                        <img src={pencils} alt="logo" width={400}/>
+                    </Grid>
+                </Grid>
+                <br/>
+                <Grid
+                    container
+                    direction="column"
+                    justify="space-between"
+                    alignItems="center"
+                >
+                    <Typography variant="h3">Start using PencilBuds!</Typography>
+                    <Paper component="form" className={classes.searchbar}>
+                        <InputBase
+                            className={classes.input}
+                            placeholder="Enter YouTube link here"
+                            inputProps={{"aria-label": "transcribe"}}
+                            value={url}
+                            onChange={handleURLChange}
+                        />
+                        <IconButton
+                            className={classes.iconButton}
+                            aria-label="search"
+                            // onClick={() => handleSubmit()}
+                            component={Link} to={{
+                            pathname: "/read",
+                            state: [{search: vidId}]
+                        }}
+                        >
+                            <SearchIcon/>
+                        </IconButton>
+                    </Paper>
+                </Grid>
+                <br/>
+                <Grid
+                    container
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center"
+                    spacing={4}
+                >
+                    <Grid item className={classes.item}>
+                        <Box width="50%">
+                            <Typography variant="h3">
+                                Since COVID, much of the learning have turned online, and
+                                countless students learn by watching online video lectures.
+                                Video lectures on websites, like YouTube, not only are easy to
+                                access, but also provides quality education for free.
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Box>
-          </Grid>
-          <Grid item className={classes.item}>
-            <img src={pencils} alt="logo" width={400} />
-          </Grid>
-        </Grid>
-        <br />
-        <Grid
-          container
-          direction="column"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Typography variant="h3">Start using PencilBuds!</Typography>
-          <Paper component="form" className={classes.searchbar}>
-            <InputBase
-              className={classes.input}
-              placeholder="Enter YouTube link here"
-              inputProps={{ "aria-label": "transcribe" }}
-              value={url}
-              onChange={handleURLChange}
-            />
-            <IconButton
-              className={classes.iconButton}
-              aria-label="search"
-              onClick={() => handleSubmit()}
-            >
-              <SearchIcon />
-            </IconButton>
-          </Paper>
-        </Grid>
-        <br />
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-          spacing={4}
-        >
-          <Grid item className={classes.item}>
-            <Box width="50%">
-              <Typography variant="h3">
-                Since COVID, much of the learning have turned online, and
-                countless students learn by watching online video lectures.
-                Video lectures on websites, like YouTube, not only are easy to
-                access, but also provides quality education for free.
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-  );
+        </Box>
+    );
 }
--- frontend/src/Read.js
+++ frontend/src/Read.js
@@ -1,8 +1,8 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import React, { useEffect, useState } from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import React, {useEffect, useState} from "react";
 import Box from "@material-ui/core/Box";
 import axios from "axios";
 
@@ -28,6 +28,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Read(props) {
+  const {id} = this.props.location;
   const classes = useStyles();
   const [title, setTitle] = useState("TITLE HERE");
   const [content, setContent] = useState("CONTENT HERE");
@@ -37,60 +38,62 @@ export default function Read(props) {
   let readingTime = 0;
 
   useEffect(() => {
-    // The config info for axios request
+    // Make a GET request with a shorthand method
     const config = {
-      method: "get",
+      method: "post",
       // put the backend endpoint here
-      url: "END_POINT_HERE",
+      url: `http://127.0.0.1:8000/summarize/${id}`,
       headers: {},
     };
 
+
     axios(config)
-      .then((response) => {
-        // get things from API
-        setTitle(response.data.title);
-        setContent(response.data.content);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then((response) => {
+          console.log(id);
+          // get things from API
+          setTitle(response.data.title);
+          setContent(response.data.content);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   });
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Box my={4} mx={8} className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography variant="h2">Notes: {title}</Typography>
-        <div className={classes.paragraph}>
-          <Typography variant="body1">{content}</Typography>
-        </div>
-      </Box>
-      <nav className={classes.drawer}>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          anchor="right"
-        >
-          <div className={classes.toolbar} />
-          <Box mx={4} my={4}>
-            {/* video title */}
-            <Typography variant="h5">{title}</Typography>
-            <hr />
-            <Typography variant="h5">Notes Info</Typography>
-            <Typography variant="body1">{paragraphs} paragraphs</Typography>
-            <Typography variant="body1">
-              {words} words, {pages} pages
-            </Typography>
-            <Typography variant="body1">
-              Reading time est.: {readingTime} minutes
-            </Typography>
-          </Box>
-        </Drawer>
-      </nav>
-    </div>
+      <div className={classes.root}>
+        <CssBaseline/>
+        <Box my={4} mx={8} className={classes.content}>
+          <div className={classes.toolbar}/>
+          <Typography variant="h2">Notes: {title}</Typography>
+          <div className={classes.paragraph}>
+            <Typography variant="body1">{content}</Typography>
+          </div>
+        </Box>
+        <nav className={classes.drawer}>
+          <Drawer
+              className={classes.drawer}
+              variant="permanent"
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              anchor="right"
+          >
+            <div className={classes.toolbar}/>
+            <Box mx={4} my={4}>
+              {/* video title */}
+              <Typography variant="h5">{title}</Typography>
+              <hr/>
+              <Typography variant="h5">Notes Info</Typography>
+              <Typography variant="body1">{paragraphs} paragraphs</Typography>
+              <Typography variant="body1">
+                {words} words, {pages} pages
+              </Typography>
+              <Typography variant="body1">
+                Reading time est.: {readingTime} minutes
+              </Typography>
+            </Box>
+          </Drawer>
+        </nav>
+      </div>
   );
 }
